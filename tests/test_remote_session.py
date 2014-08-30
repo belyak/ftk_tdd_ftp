1b1d34a88ebb6e12c0273446f99e54f02d4b702a
Andrey Belyak <beliak@mail.ru>
2014-08-30 15:26:58 +0400

    в tearDown добавлена остановка процесса с локальным сервером

--- tests/test_remote_session.py
+++ tests/test_remote_session.py
@@ -9,6 +9,7 @@ from os import unlink
 import os
 from unittest import TestCase
 import uuid
+import signal
 from client import Client
 
 from tests.ftplibd_remote_session import server
@@ -17,6 +18,11 @@ from tests.ftplibd_remote_session import settings
 
 
 class TestRemoteSession(TestCase):
+
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        self.server_process = None
+        """:type: Process"""
 
     def setUp(self):
         self.test_file = TestFile(settings.test_file_size)
@@ -29,10 +35,11 @@ class TestRemoteSession(TestCase):
         tuned_server, self.port = server.get_tuned_server(self.ftp_root)
 
         def server_func():
-            tuned_server.serve_forever()
-        #FIXME: отановить поток по окончанию теста!
-        server_process = Process(target=server_func)
-        server_process.start()
+            tuned_server.serve_forever(handle_exit=True)
+
+        self.server_process = Process(target=server_func)
+        """:type: Process"""
+        self.server_process.start()
 
     def get_connected_client_in_binary_mode(self):
         client = Client()
@@ -42,6 +49,11 @@ class TestRemoteSession(TestCase):
         return client
 
     def tearDown(self):
+        while self.server_process.is_alive():
+            # посылаем SIGINT процессу, pyftpd умеет его обрабатывать и закрывается корректно
+            os.kill(self.server_process.pid, signal.SIGINT)
+            self.server_process.join(timeout=1)
+
         unlink(self.test_file.full_filename)
 
     def test_receive_file(self):
@@ -51,6 +63,7 @@ class TestRemoteSession(TestCase):
         """
         client = self.get_connected_client_in_binary_mode()
         code, rest, data = client.retr(self.test_file.filename)
+        q_code, q_rest = client.quit()
 
         self.assertEqual(code, 226)
         self.assertTrue(self.original_data == data)
@@ -65,6 +78,8 @@ class TestRemoteSession(TestCase):
 
         with open(os.path.join(self.ftp_root, self.remote_filename), 'rb') as f:
             stored_file_content = f.read()
+
+        code, rest = client.quit()
 
         self.assertTrue(self.original_data == stored_file_content)
 
